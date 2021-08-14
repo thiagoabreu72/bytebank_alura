bd4da9c33ac48b28993eec61a93c30a6462ec011
Thiago Abreu <thiagoabreu72@gmail.com>
2021-08-14 15:13:01 -0300

    Desenvolvimento Final o curso - Começando com o framework

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,6 +1,7 @@
 import { Transferencia } from '../models/transferencia.model'
 import { Component, EventEmitter, Output } from '@angular/core';
 import { TransferenciaService } from '../services/transferencia.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -15,7 +16,7 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
-  constructor(private service: TransferenciaService) {
+  constructor(private service: TransferenciaService, private router: Router) {
 
   }
 
@@ -25,7 +26,8 @@ export class NovaTransferenciaComponent {
 
     this.service.adicionar(valorEmitir).subscribe(resultado => {
       console.log(resultado);
-      //this.limparCampos();
+      this.limparCampos();
+      this.router.navigateByUrl('extrato')
     }, error => console.error(error));
   }
 
