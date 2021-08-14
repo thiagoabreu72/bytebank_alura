64f36b1b690034b00a268bab25a83dfe9dd6b299
Thiago Abreu <thiagoabreu72@gmail.com>
2021-08-14 12:16:46 -0300

    Primeira versão do curso - Aula 7

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,36 @@
+import { Transferencia } from '../models/transferencia.model'
+import { Component, EventEmitter, Output } from '@angular/core';
+import { TransferenciaService } from '../services/transferencia.service';
+
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();  //aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor(private service: TransferenciaService) {
+
+  }
+
+  transferir() {
+    console.log('Solicitado nova transferência.');
+    const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino };
+
+    this.service.adicionar(valorEmitir).subscribe(resultado => {
+      console.log(resultado);
+      //this.limparCampos();
+    }, error => console.error(error));
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
